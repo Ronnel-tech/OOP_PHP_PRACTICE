780642f7858c5947e7667201595d4d6a46025c62
Ronnel <logdatronnell@gmail.com>
2026-02-25 12:02:18 +0800

    what the helly

--- index.php
+++ index.php
@@ -11,3 +11,24 @@ $last_name = $names -> lname;
 echo($first_name . ' '. $last_name);
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="first_name" placeholder="Enter your First name"> <br>
+
+        <br>
+    
+        <input type="text" name="last_name" placeholder="Enter your last name"> 
+
+        <button ></button>
+    </form>
+</body>
+</html>
